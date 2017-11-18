6a3dd969b80ab3e8206996e0508d47b52719c0c3
snuffegullpung <33782914+snuffegullpung@users.noreply.github.com>
2017-11-18 14:01:05 +0100

    Update discord.js

--- discord.js
+++ discord.js
@@ -1,8 +1,8 @@
 module.exports = {
-    hookId: '',
-    hookToken: '',
-    userName:'',
-    avatarUrl:'',
+    hookId: '381419779656908802',
+    hookToken: 'mHl868iHmbM7PYHJoAfYtFseB_pu4AxchOEbIywW23zt5dWtw_0oAlpJLEecqNUO56li',
+    userName:'Weeb',
+    avatarUrl:'f7999a4b4ae4f1816962a8cda2f74feb',
     sendMessage:  function(msg) {
         var querystring = require('querystring');
         var https = require('https');
